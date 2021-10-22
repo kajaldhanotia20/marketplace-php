d428c66c59d7bc259ce82c30d250b9169d30d036
Kajal Dhanotia <38569308+kajaldhanotia20@users.noreply.github.com>
2021-10-22 13:20:43 -0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,13 +2,13 @@
 $servername = "ls-1c31f2d025ebc54e33379b49252559964309ae0e.cwaaley7zj90.us-east-2.rds.amazonaws.com";
 $username = "dbmasteruser";
 $password = "v*&Z[lBm-fA_]W.#f5[S=^uaDAJJ8c)t";
-$dbname = "marketplace-database";
+$dbname = "dbmaster";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-/*if ($conn->connect_error) {
+if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
- echo "Connected successfully";*/
+ echo "Connected successfully";
 ?>
