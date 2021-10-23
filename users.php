e8bce611251ff30aa50a1da6baed9b711b7b83b3
Kajal Dhanotia <38569308+kajaldhanotia20@users.noreply.github.com>
2021-10-23 00:17:23 -0700

    Update users.php

--- users.php
+++ users.php
@@ -1,5 +1,18 @@
 <?php
 
+$servername = "ls-1c31f2d025ebc54e33379b49252559964309ae0e.cwaaley7zj90.us-east-2.rds.amazonaws.com";
+$username = "dbmasteruser";
+$password = "v*&Z[lBm-fA_]W.#f5[S=^uaDAJJ8c)t";
+$dbname = "dbmaster";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+ echo "Connected successfully";
+
 $sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
